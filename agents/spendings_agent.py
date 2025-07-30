0ede261e8077bf40ee0dff59d12bff906a481dce
Mohib Ali Khan <mohib.alikhan@systemsltd.com>
2025-07-30 10:19:07 +0500

    Pulling the new code for spendings agent

--- agents/spendings_agent.py
+++ agents/spendings_agent.py
@@ -1,2 +1,15 @@
+import pandas as pd
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+
+from langgraph.graph import StateGraph
+from dotenv import load_dotenv
+
+load_dotenv()
+
+df = pd.read_csv("C:/Users/mohib.alikhan/Desktop/Banking-Agent/Banking_Data.csv")
+
+
+
 if __name__ == "__main__":
     print("Hello this is spendings agent")
